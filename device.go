5d2c88836d1e28dcb6a2b4925b36ced40ddd8560
Peter Wood <peter@alastria.net>
2020-05-05 13:20:38 +0100

    Add missing Godoc for device.

--- device.go
+++ device.go
@@ -1,14 +1,19 @@
 package da
 
+// Abstraction of a device available on a gateway
 type Device struct {
-	Gateway      Gateway
-	Identifier   Identifier
+	// Gateway which owns this device.
+	Gateway Gateway
+	// Identifier which is used to refer to this device.
+	Identifier Identifier
+	// Capabilities this device supports.
 	Capabilities []Capability
 }
 
-func (d Device) HasCapability(needle Capability) bool {
+// Check to see if a device supports a particular capability.
+func (d Device) HasCapability(capability Capability) bool {
 	for _, haystack := range d.Capabilities {
-		if haystack == needle {
+		if haystack == capability {
 			return true
 		}
 	}
