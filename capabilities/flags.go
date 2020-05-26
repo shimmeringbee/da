c616b477387b6ab2f72443547d92ec11bd6a2b3c
Peter Wood <peter@alastria.net>
2020-05-26 14:21:01 +0100

    Correct flag name.

--- capabilities/flags.go
+++ capabilities/flags.go
@@ -6,8 +6,8 @@ import (
 
 const (
 	/* Basic capabilities to permit management of devices. */
-	DeviceDiscoveryFlag       = Capability(0x0000)
-	EnumerateCapabilitiesFlag = Capability(0x0001)
+	DeviceDiscoveryFlag = Capability(0x0000)
+	EnumerateDeviceFlag = Capability(0x0001)
 
 	/* Functional capabilities of devices. */
 )
