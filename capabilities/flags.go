06a7815ca71c8afbfbf4cd3cca28daad2b7d2340
Peter Wood <peter@alastria.net>
2020-05-28 10:53:01 +0100

    Add capability for returning product information.

--- capabilities/flags.go
+++ capabilities/flags.go
@@ -6,8 +6,9 @@ import (
 
 const (
 	/* Basic capabilities to permit management of devices. */
-	DeviceDiscoveryFlag = Capability(0x0000)
-	EnumerateDeviceFlag = Capability(0x0001)
+	DeviceDiscoveryFlag       = Capability(0x0000)
+	EnumerateDeviceFlag       = Capability(0x0001)
+	HasProductInformationFlag = Capability(0x0002)
 
 	/* Functional capabilities of devices. */
 )
---
+++ capabilities/has_product_information.go
@@ -0,0 +1,26 @@
+package capabilities
+
+import (
+	"context"
+	. "github.com/shimmeringbee/da"
+)
+
+type ProductInformationPresent uint
+
+const (
+	Manufacturer ProductInformationPresent = 1 << iota
+	Name
+	Serial
+)
+
+type ProductInformation struct {
+	Present ProductInformationPresent
+
+	Manufacturer string
+	Name         string
+	Serial       string
+}
+
+type HasProductInformation interface {
+	ProductInformation(context.Context, Device) (ProductInformation, error)
+}
