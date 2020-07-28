c6df75475bfc84c666750a4470d23c52b71e9f00
Peter Wood <peter@alastria.net>
2020-07-28 22:24:42 +0100

    Add new potential capability flags.

--- capabilities/flags.go
+++ capabilities/flags.go
@@ -9,12 +9,30 @@ const (
 	DeviceDiscoveryFlag       = Capability(0x0000)
 	EnumerateDeviceFlag       = Capability(0x0001)
 	HasProductInformationFlag = Capability(0x0002)
+	PowerStatusFlag           = Capability(0x0100)
+	ConnectivityFlag          = Capability(0x0100)
 
-	/* Functional capabilities of devices. */
-	OnOffFlag = Capability(0x1000)
+	/* Capabilities to change the physical state of devices. */
+	OnOffFlag            = Capability(0x1000)
+	LevelFlag            = Capability(0x1001)
+	ColorTemperatureFlag = Capability(0x1002)
+
+	/* Capabilities to read information from the environment. */
+	TemperatureSensorFlag  = Capability(0x2000)
+	HumiditySensorFlag     = Capability(0x2001)
+	PressureSensorFlag     = Capability(0x2002)
+	PresenceSensorFlag     = Capability(0x2003)
+	IlluminationSensorFlag = Capability(0x2004)
+
+	/* Capabilities to read information from power systems. */
+	EnergyMeasurementFlag = Capability(0x2100)
+
+	/* Alarm Systems. */
+	AlarmZoneFlag          = Capability(0x3000)
+	AlarmWarningDeviceFlag = Capability(0x3001)
 
 	/* Developer Functionality. */
-	LocalDebugFlag          = Capability(0xfff0)
-	RemoteDebugFlag         = Capability(0xfff1)
-	MessageCaptureDebugFlag = Capability(0xfff2)
+	LocalDebugFlag          = Capability(0xff00)
+	RemoteDebugFlag         = Capability(0xff01)
+	MessageCaptureDebugFlag = Capability(0xff20)
 )
