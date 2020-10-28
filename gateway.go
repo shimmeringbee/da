a927e74a717ae5de9ed87accc6bb99093057e464
Peter Wood <peter@alastria.net>
2020-10-28 19:54:45 +0000

    Fix Godoc, and add Capabilities function to interface.

--- gateway.go
+++ gateway.go
@@ -1,20 +1,25 @@
 package da
 
-import "context"
+import (
+	"context"
+)
 
-// A gateway is a provider of devices and Capability objects to control them
+// Gateway is a provider of devices and Capability objects to control them
 type Gateway interface {
-	// Read events from the gateway about devices, this must always be serviced. context.DeadlineExceeded will be
+	// ReadEvent from the gateway about devices, this must always be serviced. context.DeadlineExceeded will be
 	// returned if the context expires during a read. Use this mechanism if you can not indefinitely wait for an event.
 	ReadEvent(context.Context) (interface{}, error)
 
-	// Retrieve a capability implementation from the gateway, may return nil if capability is not supported.
+	// Capability gets a capability implementation from the gateway, may return nil if capability is not supported.
 	Capability(Capability) interface{}
 
-	// Fetch a device which represents the gateway its self, this allows a gateway to have optional capabilities.
+	// Capabilities returns a list of all Capabilities that the gateway can support.
+	Capabilities() []Capability
+
+	// Self fetches a device which represents the gateway, this allows a gateway to have optional capabilities.
 	Self() Device
 
-	// Fetch a list of all devices on the gateway, including its self.
+	// Devices fetch a list of all devices on the gateway, including its self.
 	Devices() []Device
 
 	// Start the gateway, including any background routines required.
