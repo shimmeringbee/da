6080612fd5cfe59026a237e53987b945917be46f
Peter Wood <peter@alastria.net>
2020-11-03 19:30:48 +0000

    Add capability for power supply details for device.

---
+++ capabilities/power_supply.go
@@ -0,0 +1,67 @@
+package capabilities
+
+import (
+	"context"
+	"github.com/shimmeringbee/da"
+)
+
+// PowerSupply exposes how a device is powered.
+type PowerSupply interface {
+	// Status provides the devices current state of the devices power supply
+	Status(context.Context, da.Device) (PowerStatus, error)
+}
+
+// PowerStatus represents the state of all power sources for the device.
+type PowerStatus struct {
+	// Mains represents any mains powered supplies, there may be multiple if a device has A/B supplies.
+	Mains []PowerMainsStatus
+	// Battery represents any battery supplies, there may be multiple.
+	Battery []PowerBatteryStatus
+}
+
+type PowerStatusPresent uint
+
+const (
+	Voltage PowerStatusPresent = 1 << iota
+	Frequency
+	NominalVoltage
+	Remaining
+	Available
+)
+
+// PowerMainsStatus describes a mains power supply for the device.
+type PowerMainsStatus struct {
+	// Voltage of mains supply.
+	Voltage float64
+	// Frequency of mains supply, 0 if DC.
+	Frequency float64
+	// Available represents if the supply is valid and usable.
+	Available bool
+
+	// Present represents a bitmap of the fields present.
+	Present PowerStatusPresent
+}
+
+// PowerBatteryStatus describes a battery power supply for the device.
+type PowerBatteryStatus struct {
+	// Voltage of battery.
+	Voltage float64
+	// NominalVoltage of battery.
+	NominalVoltage float64
+
+	// Remaining ratio of battery, 0.0 at empty, 1.0 at full charge.
+	Remaining float64
+	// Available represents if the supply is valid and usable.
+	Available bool
+
+	// Present represents a bitmap of the fields present.
+	Present PowerStatusPresent
+}
+
+// PowerStatusUpdate is sent to inform consumers that an update to the power supply state of the device has changed.
+type PowerStatusUpdate struct {
+	// Device which has an update.
+	Device da.Device
+	// PowerStatus of device.
+	PowerStatus PowerStatus
+}
