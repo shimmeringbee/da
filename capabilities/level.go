ba12901582a7f16f0e14ba632da09934a5c9dbe6
Peter Wood <peter@alastria.net>
2020-12-19 22:58:25 +0000

    Implement missing status on capability.

--- capabilities/level.go
+++ capabilities/level.go
@@ -11,4 +11,16 @@ import (
 type Level interface {
 	// ChangeTo changes the devices current output level to the new required value, over a set duration.
 	ChangeTo(context.Context, Device, float64, time.Duration) error
+	// Status returns the current status of Level.
+	Status(context.Context, Device) (LevelStatus, error)
+}
+
+// LevelStatus represents the current level.
+type LevelStatus struct {
+	// CurrentLevel is the current level of the device.
+	CurrentLevel float64
+	// TargetLevel is the target level of the device.
+	TargetLevel float64
+	// DurationRemaining is how long until CurrentLevel transitions to TargetLevel.
+	DurationRemaining time.Duration
 }
