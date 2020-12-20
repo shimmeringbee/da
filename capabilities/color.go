07dba134142fc352b459432da7bc4d5e9216d49a
Peter Wood <peter@alastria.net>
2020-12-20 10:35:21 +0000

    Add missing fields to report color temperature. Change ChangeTo to just Change.

--- capabilities/color.go
+++ capabilities/color.go
@@ -7,6 +7,18 @@ import (
 	"time"
 )
 
+// NativeColorspace describes the native color space of a convertible color.
+type NativeColorspace uint8
+
+const (
+	// HueSat the color is represented as hue and saturation of the sRGB colorspace.
+	HueSat NativeColorspace = 0
+	// XY the color is represented as the XY coordinates fo the CIE 1931 colorspace.
+	XY NativeColorspace = 1
+	// SRGB the color is represented as red, green and blue in the screen colorspace.
+	SRGB NativeColorspace = 2
+)
+
 // ConvertibleColor is an interface that allows acceptance of a color implementation that supports
 // modes required by a device.
 type ConvertibleColor interface {
@@ -16,13 +28,17 @@ type ConvertibleColor interface {
 	XY() (float64, float64)
 	// Color returns a Go color representing sRGB, converted from an internal format if required.
 	Color() color.Color
+	// NativeColorspace returns the native colour space of the colour.
+	NativeColorspace() NativeColorspace
 }
 
+// Color is a capability that provides the ability for a device to set the color of an output, this
+// is usually a bulb or light strip.
 type Color interface {
-	// ChangeToTemperature changes the output of the device to be that color over the duration specified.
-	ChangeToColor(context.Context, Device, ConvertibleColor, time.Duration) error
-	// ChangeToTemperature changes the output of the device to be that temperature over the duration specified.
-	ChangeToTemperature(context.Context, Device, float64, time.Duration) error
+	// ChangeTemperature changes the output of the device to be that color over the duration specified.
+	ChangeColor(context.Context, Device, ConvertibleColor, time.Duration) error
+	// ChangeTemperature changes the output of the device to be that temperature over the duration specified.
+	ChangeTemperature(context.Context, Device, float64, time.Duration) error
 	// SupportsColor if the device supports outputting a color.
 	SupportsColor() bool
 	// SupportsTemperature if the device supports outputting a specific white temperature.
@@ -31,12 +47,34 @@ type Color interface {
 	Status(context.Context, Device) (ColorStatus, error)
 }
 
+// Mode represents what the device is set to output.
+type Mode uint8
+
+const (
+	// ColorMode the device is outputting a color.
+	ColorMode Mode = 1
+	// TemperatureMode the device is outputting a black body temperature color.
+	TemperatureMode Mode = 2
+)
+
 // ColorStatus represents the current color.
 type ColorStatus struct {
-	// CurrentColor is the current color of the device.
-	CurrentColor ConvertibleColor
-	// TargetColor is the target color of the device.
-	TargetColor ConvertibleColor
-	// DurationRemaining is how long until CurrentColor transitions to TargetColor.
+	// Mode is the current color mode.
+	Mode Mode
+	// Color information if the Mode is ColorMode.
+	Color struct {
+		// Current color of the device.
+		Current ConvertibleColor
+		// Target color of the device.
+		Target ConvertibleColor
+	}
+	// Temperature information if the Mode is TemperatureMode.
+	Temperature struct {
+		// Current temperature of the device.
+		Current float64
+		// Target temperature of the device.
+		Target float64
+	}
+	// DurationRemaining is how long until Current transitions to Target.
 	DurationRemaining time.Duration
 }
--- capabilities/level.go
+++ capabilities/level.go
@@ -9,8 +9,8 @@ import (
 // Level is a capability that provides the ability for a device to set the level of an output, this
 // could be brightness, fan intensity, blind height, desk height, etc.
 type Level interface {
-	// ChangeTo changes the devices current output level to the new required value, over a set duration.
-	ChangeTo(context.Context, Device, float64, time.Duration) error
+	// Change changes the devices current output level to the new required value, over a set duration.
+	Change(context.Context, Device, float64, time.Duration) error
 	// Status returns the current status of Level.
 	Status(context.Context, Device) (LevelStatus, error)
 }
